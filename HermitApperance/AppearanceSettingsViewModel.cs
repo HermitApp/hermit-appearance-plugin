b38a3ee56753bc2d13fd31b191ae7a0ba84e2cb4
Lucas Brendel <lucasbrendel@gmail.com>
2014-12-29 19:27:21 -0600

    First commit of project

---
+++ HermitApperance/AppearanceSettingsViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Hermit.Plugin;
+using Caliburn.Micro;
+using MahApps.Metro;
+using System.Windows.Media;
+
+namespace HermitAppearance
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Export(typeof(IPlugin))]
+    public class AppearanceSettingsViewModel : SettingsItem
+    {
+        public AppearanceSettings Appearance { get; set; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public AppearanceSettingsViewModel()
+        {
+            this.DisplayName = "Appearance";
+
+            Appearance = new AppearanceSettings();
+
+            this.Appearance.AccentColors = ThemeManager.Accents
+                                            .Select(a => new AccentColorData() { Name = a.Name, ColorBrush = a.Resources["AccentColorBrush"] as Brush })
+                                            .ToList();
+
+            this.Appearance.ThemeColors = ThemeManager.AppThemes
+                                           .Select(a => new ThemeColorData() { Name = a.Name, BorderColorBrush = a.Resources["BlackColorBrush"] as Brush, ColorBrush = a.Resources["WhiteColorBrush"] as Brush })
+                                           .ToList();
+        }
+
+        
+    }
+}
